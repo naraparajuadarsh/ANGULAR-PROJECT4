0058df2a0e2b68ec9b4d61e251b88f732db3c7c0
naraparajuadarsh <adarshnaraparaju@gmail.com>
2021-08-15 16:20:25 +0530

    form completed

--- src/app/my-form/my-form.component.ts
+++ src/app/my-form/my-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MyProjectService } from '../my-project.service';
 
 @Component({
   selector: 'app-my-form',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-form.component.css']
 })
 export class MyFormComponent implements OnInit {
+  myfriends:FormGroup;
 
-  constructor() { }
+  formArray:any;
+
+  constructor(private myservice:MyProjectService) {
+    this.myfriends=new FormGroup({
+      name:new FormControl(null,Validators.required),
+      id:new FormControl(null,Validators.required),
+      year:new FormControl(null,Validators.required),
+      color:new FormControl(null,Validators.required),
+      pantone_value:new FormControl(),
+      cars:new FormArray([])
+    })
+   }
+
+   addcars()
+    {
+      (<FormArray>this.myfriends.controls.cars).push(
+            new FormGroup({
+              name:new FormControl(),
+              cars:new FormControl(),
+              age:new FormControl()
+            })
+      )
+        console.log(this.myfriends.controls.cars.value)
+    }
+
+   submit(){
+    console.log(this.myfriends)
+    this.myservice.form(this.myfriends.value).subscribe(Value=>{
+      alert("sucess")
+    },
+    error=>{
+      alert("fail")
+    }) 
+  }
 
   ngOnInit(): void {
+    
   }
 
 }
--- src/app/my-project.service.ts
+++ src/app/my-project.service.ts
@@ -6,12 +6,12 @@ import { Observable, of } from 'rxjs';
   providedIn: 'root'
 })
 export class MyProjectService {
-  objects:any=[{"id":1,"name":"cerulean","year":2000,"color":"#98B2D1","pantone_value":"15-4020"},
-                {"id":2,"name":"fuchsia rose","year":2001,"color":"#C74375","pantone_value":"17-2031"},
-                {"id":3,"name":"true red","year":2002,"color":"#BF1932","pantone_value":"19-1664"},
-                {"id":4,"name":"aqua sky","year":2003,"color":"#7BC4C4","pantone_value":"14-4811"},
-                {"id":5,"name":"tigerlily","year":2004,"color":"#E2583E","pantone_value":"17-1456"},
-                {"id":6,"name":"blue turquoise","year":2005,"color":"#53B0AE","pantone_value":"15-5217"}]
+  objects:any=[{"id":1,"name":"Adarsh","year":2000,"color":"#98B2D1","pantone_value":"15-4020"},
+                {"id":2,"name":"V Sagar","year":2001,"color":"#C74375","pantone_value":"17-2031"},
+                {"id":3,"name":"Prasad","year":2002,"color":"#BF1932","pantone_value":"19-1664"},
+                {"id":4,"name":"Ramesh","year":2003,"color":"#7BC4C4","pantone_value":"14-4811"},
+                {"id":5,"name":"Rohit","year":2004,"color":"#E2583E","pantone_value":"17-1456"},
+                {"id":6,"name":"Naveen","year":2005,"color":"#53B0AE","pantone_value":"15-5217"}]
   constructor(private httpClient:HttpClient) { }
 
   gettable():Observable<any>{
@@ -23,6 +23,12 @@ export class MyProjectService {
     console.log("index",index,this.objects)
     this.objects.splice(index,1)
     console.log(this.objects)
+    return of(true)
+  }
+
+  form(value:any):Observable<any>{
+    console.log(value)
+    this.objects.push(value)
     return of(true)
   }
 
